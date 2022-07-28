6f41a10257f4c8402abc5f89f2d8a41f4f142e49
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-07-28 17:57:54 +0100

    Update sql_coding_hr.sql

--- sql_coding_hr.sql
+++ sql_coding_hr.sql
@@ -23,3 +23,9 @@ select city, length(city) from station order by length(city) asc, city limit 1; 
 
 # Find all the city names in the table that starts with a,e,i,o,u
 SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou].*";
+
+
+# Display the top 5 max salaries 
+
+Select * from ( Select * from employee order by salary desc ) where rownum<=5;
+
