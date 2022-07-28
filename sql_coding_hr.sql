8c6f67e1e3faef9244324a32e1f33bf7c8d3cdae
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-07-28 16:27:17 +0100

    Update sql_coding_hr.sql

--- sql_coding_hr.sql
+++ sql_coding_hr.sql
@@ -1,1 +1,17 @@
+# Contains coding practice sql
 
+# find out the cities with the highest and lowest length of their names. 
+# in oracle we have rownum which makes it easier like below 
+SELECT *
+FROM
+    (SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY), CITY)
+WHERE ROWNUM = 1
+UNION
+SELECT *
+FROM
+    (SELECT CITY, LENGTH(CITY)
+    FROM STATION
+    ORDER BY LENGTH(CITY) DESC, CITY)
+WHERE ROWNUM = 1;
