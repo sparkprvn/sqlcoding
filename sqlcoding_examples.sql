697cf69637b51997afdb79b21a3d88fff2c51062
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-07-28 14:56:21 +0100

    Update sqlcoding_examples.sql

--- sqlcoding_examples.sql
+++ sqlcoding_examples.sql
@@ -1,1 +1,6 @@
 # adding sql queries as part of coding challenges
+
+#Examples from W3Schools 
+#From the following tables, write a SQL query to find the first name, last name, department number, and department name for each employee
+
+select e.FIRST_NAME, e.LAST_NAME,e.DEPARTMENT_ID,d.DEPARTMENT_NAME from employees e INNER JOIN departments d WHERE e.DEPARTMENT_ID = d.DEPARTMENT_ID;
