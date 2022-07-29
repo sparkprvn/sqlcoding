f3610a35da35d44835cf1e5c73866f8c66119d3e
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-07-29 20:02:42 +0100

    Update sql_coding_hr.sql

--- sql_coding_hr.sql
+++ sql_coding_hr.sql
@@ -29,3 +29,10 @@ SELECT DISTINCT city FROM station WHERE city REGEXP "^[aeiou].*";
 
 Select * from ( Select * from employee order by salary desc ) where rownum<=5;
 
+
+# retrieve employees that dont have a manager
+SELECT E.first_name, E.last_name, E.department_id, D.department_name 
+  FROM employees E 
+   LEFT OUTER JOIN departments D 
+     ON E.department_id = D.department_id;
+
