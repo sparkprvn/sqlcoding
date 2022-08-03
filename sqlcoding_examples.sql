93f7f5fb9de629669cffb5b25a84dd05fb4663d2
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-08-03 15:59:21 +0100

    Update sqlcoding_examples.sql

--- sqlcoding_examples.sql
+++ sqlcoding_examples.sql
@@ -66,3 +66,43 @@ select customer_number
 from orders
 group by customer_number
 order by count(*) desc limit 1
+
+
+# leetcode 
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table has no primary key, it can have repeated rows.
+product_id is a foreign key to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+
+select distinct p1.product_id, p1.product_name
+from Product p1, Sales s1
+where p1.product_id not in(
+select product_id
+from Sales
+where sale_date not between '2019-01-01' and '2019-03-31')
+and p1.product_id = s1.product_id
