f62f3dfff880eb200b19def8ec334660a5d04a5e
Praveen Kumar <35918752+sparkprvn@users.noreply.github.com>
2022-08-03 16:14:06 +0100

    Update sqlcoding_examples.sql

--- sqlcoding_examples.sql
+++ sqlcoding_examples.sql
@@ -106,3 +106,62 @@ select product_id
 from Sales
 where sale_date not between '2019-01-01' and '2019-03-31')
 and p1.product_id = s1.product_id
+
+# leet code 
+Table: SalesPerson
+
++-----------------+---------+
+| Column Name     | Type    |
++-----------------+---------+
+| sales_id        | int     |
+| name            | varchar |
+| salary          | int     |
+| commission_rate | int     |
+| hire_date       | date    |
++-----------------+---------+
+sales_id is the primary key column for this table.
+Each row of this table indicates the name and the ID of a salesperson alongside their salary, commission rate, and hire date.
+ 
+
+Table: Company
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| com_id      | int     |
+| name        | varchar |
+| city        | varchar |
++-------------+---------+
+com_id is the primary key column for this table.
+Each row of this table indicates the name and the ID of a company and the city in which the company is located.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| order_id    | int  |
+| order_date  | date |
+| com_id      | int  |
+| sales_id    | int  |
+| amount      | int  |
++-------------+------+
+order_id is the primary key column for this table.
+com_id is a foreign key to com_id from the Company table.
+sales_id is a foreign key to sales_id from the SalesPerson table.
+Each row of this table contains information about one order. This includes the ID of the company, the ID of the salesperson, the date of the order, and the amount paid.
+ 
+
+Write an SQL query to report the names of all the salespersons who did not have any orders related to the company with the name "RED".
+
+Return the result table in any order.
+
+select distinct name 
+from SalesPerson  
+where name not in 
+(select distinct S.name 
+from SalesPerson S, Orders O, Company C
+where C.name = "RED" and O.com_id = C.com_id and S.sales_id = O.sales_id)
+
+
